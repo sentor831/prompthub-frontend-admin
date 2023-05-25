b98fcc31c7584082d00dce4ad8e63cdbcd6e7ef9
侯雨霏 <1078007247@qq.com>
2023-05-25 11:09:26 +0800

    rename img to image

--- src/pages/UserProfile/UserCard.vue
+++ src/pages/UserProfile/UserCard.vue
@@ -1,9 +1,10 @@
 <template>
   <card class="card-user">
-    <img slot="image" src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
+    <img slot="image" src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
+      alt="..." />
     <div class="author">
       <a href="#">
-        <img class="avatar border-gray" src="img/faces/face-3.jpg" alt="..."/>
+        <img class="avatar border-gray" src="image/faces/face-3.jpg" alt="..." />
 
         <h4 class="title">Mike Andrew<br />
           <small>michael24</small>
@@ -22,44 +23,42 @@
   </card>
 </template>
 <script>
-  import Card from 'src/components/Cards/Card.vue'
-  export default {
-    components: {
-      Card
-    },
-    data () {
-      return {
-        details: [
-          {
-            title: '12',
-            subTitle: 'Files'
-          },
-          {
-            title: '2GB',
-            subTitle: 'Used'
-          },
-          {
-            title: '24,6$',
-            subTitle: 'Spent'
-          }
-        ]
-      }
-    },
-    methods: {
-      getClasses (index) {
-        var remainder = index % 3
-        if (remainder === 0) {
-          return 'col-md-3 col-md-offset-1'
-        } else if (remainder === 2) {
-          return 'col-md-4'
-        } else {
-          return 'col-md-3'
+import Card from 'src/components/Cards/Card.vue'
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      details: [
+        {
+          title: '12',
+          subTitle: 'Files'
+        },
+        {
+          title: '2GB',
+          subTitle: 'Used'
+        },
+        {
+          title: '24,6$',
+          subTitle: 'Spent'
         }
+      ]
+    }
+  },
+  methods: {
+    getClasses(index) {
+      var remainder = index % 3
+      if (remainder === 0) {
+        return 'col-md-3 col-md-offset-1'
+      } else if (remainder === 2) {
+        return 'col-md-4'
+      } else {
+        return 'col-md-3'
       }
     }
   }
+}
 
 </script>
-<style>
-
-</style>
+<style></style>
